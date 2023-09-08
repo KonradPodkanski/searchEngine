a9fd3906f6a163183979b8658494d61c8f17219e
unknown <podkanski050@gmail.com>
2023-09-08 18:28:57 +0200

    created search programme

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+const searchInput = document.querySelector("#search");
+const listElements = document.querySelectorAll("ul li");
+
+const chooseDrink = e => {
+	for (let beverage of listElements) {
+		if (!beverage.textContent.toLowerCase().includes(e.target.value.toLowerCase())) {
+			beverage.style.display = "none";
+		} else {
+			beverage.style.display = "block";
+		}
+	}
+};
+
+searchInput.addEventListener("keyup", chooseDrink);
